841fef62fb8886ec6757a67c0dea6dc20ee4ad78
sidahmed <benaissas886@gmail.com>
2021-12-20 12:43:56 +0100

    First commit

---
+++ _build/parser.ml
@@ -0,0 +1,125 @@
+
+module MenhirBasics = struct
+  
+  exception Error
+  
+  type token = 
+    | Lint of (
+# 6 "parser.mly"
+       (int)
+# 11 "parser.ml"
+  )
+    | Lend
+  
+end
+
+include MenhirBasics
+
+let _eRR =
+  MenhirBasics.Error
+
+type _menhir_env = {
+  _menhir_lexer: Lexing.lexbuf -> token;
+  _menhir_lexbuf: Lexing.lexbuf;
+  _menhir_token: token;
+  mutable _menhir_error: bool
+}
+
+and _menhir_state
+
+# 1 "parser.mly"
+  
+  open Ast
+  open Ast.Syntax
+
+# 36 "parser.ml"
+
+let rec _menhir_discard : _menhir_env -> _menhir_env =
+  fun _menhir_env ->
+    let lexer = _menhir_env._menhir_lexer in
+    let lexbuf = _menhir_env._menhir_lexbuf in
+    let _tok = lexer lexbuf in
+    {
+      _menhir_lexer = lexer;
+      _menhir_lexbuf = lexbuf;
+      _menhir_token = _tok;
+      _menhir_error = false;
+    }
+
+and prog : (Lexing.lexbuf -> token) -> Lexing.lexbuf -> (
+# 11 "parser.mly"
+      (Ast.Syntax.expr)
+# 53 "parser.ml"
+) =
+  fun lexer lexbuf ->
+    let _menhir_env = let _tok = Obj.magic () in
+    {
+      _menhir_lexer = lexer;
+      _menhir_lexbuf = lexbuf;
+      _menhir_token = _tok;
+      _menhir_error = false;
+    } in
+    Obj.magic (let _menhir_stack = ((), _menhir_env._menhir_lexbuf.Lexing.lex_curr_p) in
+    let _menhir_env = _menhir_discard _menhir_env in
+    let _tok = _menhir_env._menhir_token in
+    match _tok with
+    | Lint _v ->
+        let _menhir_stack = Obj.magic _menhir_stack in
+        let _startpos = _menhir_env._menhir_lexbuf.Lexing.lex_start_p in
+        let _menhir_env = _menhir_discard _menhir_env in
+        let _menhir_stack = Obj.magic _menhir_stack in
+        let (n : (
+# 6 "parser.mly"
+       (int)
+# 75 "parser.ml"
+        )) = _v in
+        let _startpos_n_ = _startpos in
+        let _v : (Ast.Syntax.expr) = 
+# 20 "parser.mly"
+           (
+  Int { value = n ; pos = _startpos_n_ }
+)
+# 83 "parser.ml"
+         in
+        let _menhir_stack = (_menhir_stack, _v) in
+        let _menhir_stack = Obj.magic _menhir_stack in
+        assert (not _menhir_env._menhir_error);
+        let _tok = _menhir_env._menhir_token in
+        (match _tok with
+        | Lend ->
+            let _menhir_stack = Obj.magic _menhir_stack in
+            let _menhir_stack = Obj.magic _menhir_stack in
+            let (_menhir_stack, (e : (Ast.Syntax.expr))) = _menhir_stack in
+            let _2 = () in
+            let _v : (
+# 11 "parser.mly"
+      (Ast.Syntax.expr)
+# 98 "parser.ml"
+            ) = 
+# 16 "parser.mly"
+                  (e)
+# 102 "parser.ml"
+             in
+            let _menhir_stack = Obj.magic _menhir_stack in
+            let _menhir_stack = Obj.magic _menhir_stack in
+            let (_1 : (
+# 11 "parser.mly"
+      (Ast.Syntax.expr)
+# 109 "parser.ml"
+            )) = _v in
+            Obj.magic _1
+        | _ ->
+            assert (not _menhir_env._menhir_error);
+            _menhir_env._menhir_error <- true;
+            let _menhir_stack = Obj.magic _menhir_stack in
+            raise _eRR)
+    | _ ->
+        assert (not _menhir_env._menhir_error);
+        _menhir_env._menhir_error <- true;
+        let _menhir_stack = Obj.magic _menhir_stack in
+        raise _eRR)
+
+# 269 "<standard.mly>"
+  
+
+# 126 "parser.ml"
